346cbd5bb167d4d00ec2f55bb5c99a56e7e0009e
haiderjutt <haiderjutt95@gmail.com>
2020-11-15 03:31:49 +0500

    shayd

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,16 +13,11 @@ class ComposerAutoloaderInit5c2553128c946a63668ca6e1aede3cac
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
-
-        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit5c2553128c946a63668ca6e1aede3cac', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -30,7 +25,7 @@ class ComposerAutoloaderInit5c2553128c946a63668ca6e1aede3cac
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit5c2553128c946a63668ca6e1aede3cac::getInitializer($loader));
         } else {
